d3be94bd11f158d40a3090a00be7986b9d71fcf9
VAISHNAVIP0419 <134695731+VAISHNAVIP0419@users.noreply.github.com>
2025-04-02 00:06:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Serving HTML file
+    return render_template('index.html')  # This should match your HTML file in templates/
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)  # Ensure it's accessible
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
